0d1e8f58bcad813acc28c5b20806f181d2919651
puspitadw <pd0027006@gmail.com>
2024-07-04 17:34:11 +0700

    .

---
+++ js/main.js
@@ -0,0 +1,379 @@
+const products = [
+    {
+        id: 1,
+        name: 'BTS - LOVE YOURSELF - Tear (3rd Album)',
+        price: 'Rp.279.900',
+        image: '../Asset/Album/Albumlystear.jpg',
+        detail: [
+            'CD',
+            '104p Photobook',
+            '20p Minibook',
+            'Random Photocard',
+            'Special Photocard',
+            'Standing Photo',
+        ]
+    },
+    {
+        id: 2,
+        name: 'BTS - BE (ESSENTIAL EDITION )',
+        price: 'Rp.355.000',
+        image: '../Asset/Album/AlbumBE.jpg',
+        detail: [
+            '1 CD',
+            'Photo book : W158 X H214 X T13 (mm) / 140 pages',
+            'Photo card 1: 7p',
+            'Photo card 2: random 1p out of 8p',
+            'Poster (folding) 1p: W642 X H471 (mm)',
+        ]
+    },
+    {
+        id: 3,
+        name: 'BTS - MAP OF THE SOUL: Persona [CD Album Original]',
+        price: 'Rp.259.900',
+        image: '../Asset/Album/Albummapofthesoulv1.jpg',
+        detail: [
+            '1 CD',
+            '76p Photobook',
+            '20p Minibook',
+            '1pcs Random Photocard',
+            '1pcs Random Postcard',
+            '1pcs Photo Film',
+            'Clear Photo Picket',
+        ]
+    },
+    {
+        id: 4,
+        name: 'Album BTS - Anthology Album PROOF STANDARD EDITION',
+        price: 'Rp.950.000',
+        image: '../Asset/Album/Albumproofstandaredition.jpg',
+
+        detail: [
+            'OUTER BOX: 1 TYPE / W 188 x L 250 x H 59 (mm)',
+            'THE ART OF PROOF: 1 TYPE / W 170 x H 238 (mm) / 132 Pages',
+            'PHOTOGRAPH: 1 TYPE / W 170 x H 238 (mm) / 104 Pages',
+            'EPILOGUE: 1 TYPE / W 170 x H 238 (mm) / 80 pages',
+            'LYRICS: 1 TYPE / W 170 x H 238 (mm) / 44 Pages',
+            'CD PLATE',
+            '3 MusicCD',
+            'PHOTOCARD A: 1 SET (7ea)',
+            'PHOTOCARD B: Random 1p out of 8p',
+            'POSTCARD : Random 1p out of 8p',
+            'POSTER: 1 TYPE / W 841 x H 564 (mm) / *1st press limited',
+        ]
+    },
+    {
+        id: 5,
+        name: 'Album BTS - BUTTER ',
+        price: 'Rp.299.900',
+        image: '../Asset/Album/AlbumButter.jpg',
+        detail: [
+            'Butter Single ver. [ Cream , Peaches ]',
+            '1 MusicCD : each version 1p',
+            'Out Box : each version 1p W 140 X L 140 X H 40 (mm)',
+            'Photobook : each version 1p W 137 X H 137 (mm) / Cream 108Pages, Peaches 104Pages',
+            'Photo card : random 1p out of each version 8p',
+            'Instant photo card : 1p ( same foe each version )',
+            'Photo stand : each version 1p W 137 X H 95 (mm)',
+            'PHOTOCARD A: 1 SET (7ea)',
+            'Folded message card : random 1p out of 7p',
+            'Graphic Sticker : each version 1p',
+            'Lyrics : 2p ( same for each version )',
+        ]
+    },
+    {
+        id: 6,
+        name: 'BTS ALBUM PROOF SET + POB WVERSE OFFICIAL ',
+        price: 'Rp.1.490.000',
+        image: '../Asset/Album/Albumproof-set.jpg',
+        detail: [
+            '1 standard edition',
+            '1 compact edition',
+            '1 triptych (foto panjang',
+            '1 acrylic card holder',
+            '1 photocard random',
+        ]
+    },
+    {
+        id: 7,
+        name: 'BTS - Official Lightstick VER 1',
+        price: 'Rp.351.000',
+        image: '../Asset/Lightstick/ARMYBOMBV1.jpg',
+        detail: [
+            'Features: Brand new and high quality',
+            'Specifications:',
+            'Material: Plastic',
+            'Minimun operating time: 6hours',
+            'Maximum output: 1W',
+            'Power by: 3PCS*AAA Batteries(not includes)',
+            'size: 112*112*242mm',
+            'Package Included: 1 x Concert Lightstick',
+        ]
+    },
+    {
+        id: 8,
+        name: 'BTS - Official Lightstick VER 2',
+        price: 'Rp.368.000',
+        image: '../Asset/Lightstick/ARMYBOMBV2.jpg',
+        detail: [
+            'battery : 3 AAA',
+            'Size : 98x226mm',
+            'package included :',
+            '- Light Stick',
+            '- Sling',
+            '- Instruction',
+            '- Bag',
+        ]
+    },
+    {
+        id: 9,
+        name: 'BTS - Official Lightstick VER 3',
+        price: 'Rp.450.000',
+        image: '../Asset/Lightstick/ARMYBOMBV3.jpg',
+        detail: [
+            'Spesifikasi:',
+            '- Bahan : Plastik',
+            '- Versi : 3',
+            '- Ukuran produk : 9.8*9.8*22.4 cm',
+            '- Ukuran kotak : 23.5*10*10 cm',
+            '- Daya : 3pcs batu baterai AAA (tidak termasuk)',
+            'Isi kemasan:',
+            '- 1 BTS Lightstick',
+            '- 1 Tali / Gantungan Lightstick',
+            '- 1 Tas Penyimpanan Lighstick',
+            '- 7 Photocard member BTS',
+            '- 1 Box Lightstick',
+            '- 1 Buku Petunjuk',
+            '- 1 Kartu (without point and QR)',
+        ]
+    },
+    {
+        id: 10,
+        name: 'BTS - Official Lightstick Special Edition VER 4',
+        price: 'Rp.800.000',
+        image: '../Asset/Lightstick/ARMYBOMBV4.jpg',
+        detail: [
+            'ORI co f/ Weverse Shop',
+            'LIGHT STICK',
+            'DUST BAG',
+            'STRAP',
+            'CASE',
+            'PHOTOCARD ALL MEMBER',
+            'WARRANTY CARD',
+            'AAA AKLINE BATTERIES X 3EA (NOT INCLUDE)',
+            'CONNECT TO BLUETOOTH',
+        ]
+    },
+    {
+        id: 11,
+        name: 'BONEKA BANTAL BT21 (CHIMMY)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekachimmy.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 12,
+        name: 'BONEKA BANTAL BT21 (RJ)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekarj.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 13,
+        name: 'BONEKA BANTAL BT21 (KOYA)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekakoya.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 14,
+        name: 'BONEKA BANTAL BT21 (SHOOKY)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekashooky.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 15,
+        name: 'BONEKA BANTAL BT21 (TATA)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekatata.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 16,
+        name: 'BONEKA BANTAL BT21 (MANG)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekamang.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 17,
+        name: 'BONEKA BANTAL BT21 (COOKY)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekacooky.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+    {
+        id: 18,
+        name: 'BONEKA BANTAL BT21 (VAN)',
+        price: 'Rp.40.000',
+        image: '../Asset/boneka/bonekavan.jpg',
+        detail: [
+            'Ukuran :40 cm',
+            'Bahan : 100 polyester fiber, Filling: 100% polyester fiber',
+        ]
+    },
+]
+function createHeader() {
+    let header = document.querySelector('.header');
+    header.innerHTML = `
+        <h1>BTS Merchandise Store</h1>
+        <nav>
+            <ul>
+                <li><a href="#home">Beranda</a></li>
+                <li><a href="#products">Produk</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    `;
+}
+
+function createHomePage() {
+    let content = document.querySelector('.content');
+    let homeSection = document.createElement('section');
+    homeSection.id = 'home';
+    homeSection.innerHTML = `
+        <div class="home-banner">
+            <h2>Welcome to BTS Merchandise Store</h2>
+            <p>Your one-stop shop for all official BTS merchandise.</p>
+            <button id="shop-now">Shop Now</button>
+        </div>
+    `;
+    content.appendChild(homeSection);
+    document.getElementById('shop-now').addEventListener('click', () => {
+        window.location.href = '#products';
+    });
+}
+
+function card(product) {
+    let content = document.querySelector('.product-grid');
+    let poductSection = document.createElement('section');
+    poductSection.id = 'products';
+    let figure = document.createElement('figure');
+    let figcaption = document.createElement('figcaption');
+    let h1 = document.createElement('h1');
+    h1.textContent = product.name;
+    let img = document.createElement('img');
+    img.src = product.image;
+    let h2 = document.createElement('h2');
+    h2.textContent = product.price;
+    let ul = document.createElement('ul');
+    let h4 = document.createElement('h4');
+    h4.textContent = "Detail Product :";
+    product.detail.map((d) => {
+        let li = document.createElement('li');
+        li.textContent = d;
+        ul.appendChild(li);
+    })
+    let button1 = document.createElement('button');
+    button1.textContent = "Beli sekarang";
+    button1.addEventListener('click', () => {
+        confirm('Apakah anda yakin ingin beli produk ini?');
+    })
+    let button2 = document.createElement('button');
+    button2.textContent = "Masukkan Ke Keranjang";
+    button2.addEventListener('click', () => {
+        alert('Produk dimasukkan ke keranjang anda');
+    })
+    let button3 = document.createElement('button');
+    button3.textContent = "Coment";
+    button3.addEventListener('click', () => {
+        prompt("Silahkan isi komentar anda");
+    })
+    figcaption.appendChild(h1);
+    figcaption.appendChild(h2);
+    figcaption.appendChild(h4);
+    figcaption.appendChild(ul);
+    figcaption.appendChild(button1);
+    figcaption.appendChild(button2);
+    figcaption.appendChild(button3);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    content.appendChild(poductSection);
+    content.appendChild(figure);
+
+}
+
+function createFooter() {
+    let content = document.querySelector('.footer');
+    let footerSection = document.createElement('section');
+    footerSection.id = 'contact';
+    footerSection.innerHTML = `
+        <section class="footer-content">
+            <div class="about-us">
+                <h3>About Us</h3>
+                <p>Kami adalah platform penjualan merchandise BTS terkemuka yang menyediakan akses mudah
+                untuk menemukan dan membeli produk BTS favorit Anda secara online. Nikmati pengalaman berbelanja
+                yang lebih nyaman dan praktis dengan layanan kami yang cepat dan andal.</p>
+            </div>
+            <div class="contact-info">
+                <h3>Contact</h3>
+                <p>Alamat: Jl. Bissmillah No. 123, Bandung, Indonesia</p>
+                <p>Email: <a href="#">info@puspita.com</a></p>
+                <p>Telepon: +62 21 1234567</p>
+            </div>
+            <div class="social-media">
+                <h3>Follow Us</h3>
+                <div class="social-icons">
+                    <a href="#" onclick="alert('Link tidak ditemukan')" class="social-icon facebook">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="#" onclick="alert('Link tidak ditemukan')" class="social-icon twitter">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="https://www.instagram.com/pspita_dw" target="_blank" class="social-icon instagram">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" onclick="alert('Link tidak ditemukan')" class="social-icon linkedin">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="legal">
+                <p><a href="#">Kebijakan Privasi</a> | <a href="#">Syarat dan Ketentuan</a></p>
+                <p>Hak Cipta © 2024 Puspita.com. Semua Hak Dilindungi.</p>
+            </div>
+        </section>
+    `;
+    content.appendChild(footerSection);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    createHeader();
+    createHomePage();
+    products.map((product) => {
+        card(product);
+    });
+    createFooter();
+});
+
